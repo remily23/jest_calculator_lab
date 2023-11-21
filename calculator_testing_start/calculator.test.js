4d6e8fb89b001d262f05af3166421b6b11aeb298
Emily Reid <emily.reid31@gmail.com>
2023-11-21 16:38:40 +0000

    completed passing tests up to multiplying

--- calculator_testing_start/calculator.test.js
+++ calculator_testing_start/calculator.test.js
@@ -38,10 +38,56 @@ describe('sum', () => {
 });
 
 describe('subtract', () => {
+  test('can subtract two small positive numbers', () => {
+    expected = 10;
+    actual = subtract(30, 20);
+    expect(actual).toBe(expected);
+  });
+
+  test('can subtract two larger positive numbers', () => {
+    expected = 5000;
+    actual = subtract(6000, 1000);
+    expect(actual).toBe(expected);
+  });
+  
+  test('can subtract two negative numbers', () => {
+    expected = 1;
+    actual = subtract(-3, -4);
+    expect(actual).toBe(expected);
+  });
+
+  test('can subtract zero', () => {
+    expected = 5;
+    actual = subtract(5, 0);
+    expect(actual).toBe(expected);
+  });
 
 });
 
 describe('multiply', () => {
+  test('can multiply two small positive numbers', () => {
+    expected = 10;
+    actual = multiply(1, 10);
+    expect(actual).toBe(expected);
+  });
+
+  test('can multiply two large positive numbers', () => {
+    expected = 10000;
+    actual = multiply(100, 100);
+    expect(actual).toBe(expected);
+  });
+
+  test('can multiply two negative numbers', () => {
+    expected = 8;
+    actual = multiply(-2, -4);
+    expect(actual).toBe(expected);
+  });
+
+  test('can multiply by zero', () => {
+    expected = 0;
+    actual = multiply(0, 1);
+    expect(actual).toBe(expected);
+  });
 
 });
 
